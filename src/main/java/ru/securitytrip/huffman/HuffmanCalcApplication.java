b444763f9619f5b359bd9296dae5f51541e7716d
Ilia Lysov <illigiorno@gmail.com>
2025-10-03 20:16:21 +0400

    Initial commit

---
+++ src/main/java/ru/securitytrip/huffman/HuffmanCalcApplication.java
@@ -0,0 +1,13 @@
+package ru.securitytrip.huffman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuffmanCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuffmanCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/securitytrip/huffman/HuffmanCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.securitytrip.huffman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
